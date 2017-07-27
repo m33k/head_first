ac9b0cc2d7d6926e8f243dca808fe3d54ca45892
m33k <tempewillnotlast@hotmail.com>
2017-07-26 20:54:06 -0400

    Added section for the game to generate a number for the player

--- 1_starting_to_code/guessing_game.py
+++ 1_starting_to_code/guessing_game.py
@@ -1,20 +1,21 @@
 #!/bin/python3.4
 # Description: Gaming exercise
-# Version 3.1: Make game ask gamer for the number until they guess it correctly.
+# Version 4: Make game generate numbers and ask gamer for the number until they guess it correctly.
 
-answer = 5
+from random import randint
+secret = randint(1,10)
 
 guess = int(input("Guess the number: "))
 
-while guess != 5:
- if guess < 5:
+while guess != secret:
+ if guess < secret:
    print("Too Low!")
- if guess > 5:
+ if guess > secret:
    print("Too High!")
 
  guess = int(input("Guess the number: "))
 
-if guess == answer:
+if guess == secret:
   print("You got it!")
   print("Thanks for playing!")
 
